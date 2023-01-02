e2bc640e6b2ba3a98e317264ece2e813adf8e8a1
perez1187 <o.perez1187@gmail.com>
2023-01-02 14:37:21 +0100

    initial setup

---
+++ aff_api/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+from .base import env
+
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="django-insecure-9g&el5ve&&tcvm+ra&&5-_yhd-w!$pefq0^tt9t1#=6c5s^roy",
+)
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
